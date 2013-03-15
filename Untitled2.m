581056ec048cabdefec0c3674b0fe30258845a8e
Zhou Yisong <failed.zys@gmail.com>
2013-03-15 21:18:17 +0800

    get start

---
+++ Untitled2.m
@@ -0,0 +1,7 @@
+function [ Ans ] = Untitled2( input_args )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ mybwlabeldumb.m
@@ -0,0 +1,8 @@
+function [ Ans ] = mybwlabeldumb( bwmap )
+[n,m] = size(bwmap);
+for ii = 1 : n
+    for jj = 1 : m
+        
+
+end
+
